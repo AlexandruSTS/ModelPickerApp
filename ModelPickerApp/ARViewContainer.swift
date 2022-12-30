69b475595cf200ecc3d334af60b5eb7585398db0
Alex <alex@stsolution.be>
2022-12-30 16:06:14 +0100

    updated working version

--- ModelPickerApp/ARViewContainer.swift
+++ ModelPickerApp/ARViewContainer.swift
@@ -35,60 +35,6 @@ struct ARViewContainer: UIViewRepresentable {
                 print("ARContainer -> DEBUG: Unable to load model to scene - \(model.modelName)"  )
             }
         }
-        
     }
 }
-    
-    class FocusARView: ARView {
-        enum FocusStyleChoices {
-            case classic
-            case material
-            case color
-        }
-        
-        /// Style to be displayed in the example
-        let focusStyle: FocusStyleChoices = .color
-        var focusEntity: FocusEntity?
-        required init(frame frameRect: CGRect) {
-            super.init(frame: frameRect)
-            self.setupConfig()
-            
-            switch self.focusStyle {
-            case .color:
-                self.focusEntity = FocusEntity(on: self, focus: .classic)
-            case .material:
-                do {
-                    let onColor: MaterialColorParameter = try .texture(.load(named: "Add"))
-                    let offColor: MaterialColorParameter = try .texture(.load(named: "Open"))
-                    self.focusEntity = FocusEntity(
-                        on: self,
-                        style: .colored(
-                            onColor: onColor, offColor: offColor,
-                            nonTrackingColor: offColor
-                        )
-                    )
-                } catch {
-                    self.focusEntity = FocusEntity(on: self, focus: .classic)
-                    print("DEBUG: Unable to load plane textures")
-                    print(error.localizedDescription)
-                }
-            default:
-                self.focusEntity = FocusEntity(on: self, focus: .classic)
-            }
-        }
-        
-        func setupConfig() {
-            
-            let config = ARWorldTrackingConfiguration()
-            config.planeDetection = [.horizontal, .vertical ]
-            config.environmentTexturing = .automatic
-            if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh){
-                config.sceneReconstruction = .mesh
-            }
-            self.session.run(config)
-        }
-        
-        @objc required dynamic init?(coder decoder: NSCoder) {
-            fatalError("DEBUG: init(coder:) has not been implemented")
-        }
-    }
+
