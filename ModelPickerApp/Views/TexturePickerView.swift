2b7c142b4618b49afcb24ec930a828e1a9c11df1
Alex <alex@stsolution.be>
2022-12-30 16:34:37 +0100

    first commit

---
+++ ModelPickerApp/Views/TexturePickerView.swift
@@ -0,0 +1,20 @@
+//
+//  TexturePickerView.swift
+//  ModelPickerApp
+//
+//  Created by Alex Anghel on 30/12/2022.
+//
+
+import SwiftUI
+
+struct TexturePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TexturePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TexturePickerView()
+    }
+}
